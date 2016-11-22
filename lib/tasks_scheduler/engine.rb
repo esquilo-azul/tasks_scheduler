29a2bf28119da945416b015a6b888ce377189cc8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-11-22 09:57:45 -0300

    Online migrations.

--- lib/tasks_scheduler/engine.rb
+++ lib/tasks_scheduler/engine.rb
@@ -1,4 +1,9 @@
 module TasksScheduler
   class Engine < ::Rails::Engine
+    initializer :append_migrations do |app|
+      config.paths['db/migrate'].expanded.each do |expanded_path|
+        app.config.paths['db/migrate'] << expanded_path
+      end
+    end
   end
 end
