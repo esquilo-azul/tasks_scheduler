b0be55bbf8579708bab06fa1e4e0cb708aa5c7f5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-11-26 14:46:09 -0200

    Option to log daemon checker on a file.

--- lib/tasks_scheduler/checker.rb
+++ lib/tasks_scheduler/checker.rb
@@ -3,8 +3,10 @@ module TasksScheduler
     include Singleton
 
     CHECK_INTERVAL = 15
+    LOG_ON_FILE_ENV_KEY = 'TASKS_SCHEDULER_LOG_ON_FILE'.freeze
 
     def run
+      check_log
       running = true
       Signal.trap('TERM') { running = false }
       while running
@@ -13,6 +15,22 @@ module TasksScheduler
         Rails.logger.info("All tasks checked. Sleeping for #{CHECK_INTERVAL} second(s)...")
         sleep(CHECK_INTERVAL)
       end
+    end
+
+    private
+
+    def check_log
+      return unless log_on_file?
+      ::FileUtils.mkdir_p(File.dirname(log_path))
+      ::Rails.logger = ::Logger.new(log_path)
+    end
+
+    def log_on_file?
+      ENV[LOG_ON_FILE_ENV_KEY].present?
+    end
+
+    def log_path
+      ::Rails.root.join('log', 'tasks_scheduler', 'checker.log')
     end
   end
 end
