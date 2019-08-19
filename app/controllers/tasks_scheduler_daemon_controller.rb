d686558c2208bda5777a16e894dcb17b4b780599
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-18 22:59:20 -0300

    TasksSchedulerDaemonController: refact.

--- app/controllers/tasks_scheduler_daemon_controller.rb
+++ app/controllers/tasks_scheduler_daemon_controller.rb
@@ -1,4 +1,6 @@
 class TasksSchedulerDaemonController < ApplicationController
+  require_relative 'tasks_scheduler_daemon_controller/_download_log'
+
   def index
   end
 
@@ -10,25 +12,5 @@ class TasksSchedulerDaemonController < ApplicationController
   def status
     render json: { daemon_running: ::TasksScheduler::Daemon.running?,
                    tasks_all_ok: !::ScheduledTask.all.any?(&:failed?) }
-  end
-
-  def download_log
-    if File.exist?(::TasksScheduler::Checker.instance.log_path)
-      send_log_file
-    else
-      redirect_to(tasks_scheduler_daemon_path,
-                  notice: "Arquivo \"#{::TasksScheduler::Checker.instance.log_path}\" não existe")
-    end
-  end
-
-  private
-
-  def send_log_file
-    send_file(
-      ::TasksScheduler::Checker.instance.log_path,
-      filename: "#{request.base_url.parameterize}_tasks-scheduler_checker-log_" \
-        "#{Time.zone.now.to_s.parameterize}.log",
-      type: 'text/plain'
-    )
   end
 end
---
+++ app/controllers/tasks_scheduler_daemon_controller/_download_log.rb
@@ -0,0 +1,21 @@
+class TasksSchedulerDaemonController < ApplicationController
+  def download_log
+    if File.exist?(::TasksScheduler::Checker.instance.log_path)
+      send_log_file
+    else
+      redirect_to(tasks_scheduler_daemon_path,
+                  notice: "Arquivo \"#{::TasksScheduler::Checker.instance.log_path}\" não existe")
+    end
+  end
+
+  private
+
+  def send_log_file
+    send_file(
+      ::TasksScheduler::Checker.instance.log_path,
+      filename: "#{request.base_url.parameterize}_tasks-scheduler_checker-log_" \
+        "#{Time.zone.now.to_s.parameterize}.log",
+      type: 'text/plain'
+    )
+  end
+end
