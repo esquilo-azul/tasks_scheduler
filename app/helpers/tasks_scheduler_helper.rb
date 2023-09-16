f7c4739ca5b6bc5aae13b1dc97e7bb63faf8e846
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-09-16 16:47:39 -0300

    TasksSchedulerHelper.tasks_scheduler_navbar_entries: retrieve entries from Rails.application.root_menu.

--- app/helpers/tasks_scheduler_helper.rb
+++ app/helpers/tasks_scheduler_helper.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 module TasksSchedulerHelper
-  NAVBAR_ENTRIES = {
-    tasks_scheduler_daemon: 'tasks_scheduler_daemon',
-    scheduled_tasks: 'scheduled_tasks',
-    tasks_scheduler_status: :status_scheduled_tasks
-  }.freeze
-
   def tasks_scheduler_navbar
     tag.navbar do
       safe_join(tasks_scheduler_navbar_entries.map { |label, path| link_to label, path }, ' | ')
@@ -14,8 +8,8 @@ module TasksSchedulerHelper
   end
 
   def tasks_scheduler_navbar_entries
-    NAVBAR_ENTRIES
-      .map { |i18n_key, path_name| [::I18n.t(i18n_key), send("#{path_name}_path")] }
-      .to_h
+    ::Rails.application.root_menu.sub(:admin).sub(:tasks_scheduler).children
+           .map { |child| [child.label, child.view_path(self)] }
+           .to_h
   end
 end
