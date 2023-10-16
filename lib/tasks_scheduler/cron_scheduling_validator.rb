0fe57539e4390de9dca046bc9e6a316298b036e5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-10-16 15:14:12 -0300

    .rubocop.yml#Rails/DeprecatedActiveModelErrorsMethods: remove excludes.

--- lib/tasks_scheduler/cron_scheduling_validator.rb
+++ lib/tasks_scheduler/cron_scheduling_validator.rb
@@ -5,8 +5,8 @@ module TasksScheduler
     def validate_each(record, attribute, value)
       return if value_valid?(value)
 
-      record.errors[attribute] << (options[:message] ||
-          I18n.t(:cron_scheduling_validator_error_message))
+      record.errors.add(attribute, (options[:message] ||
+          I18n.t(:cron_scheduling_validator_error_message)))
     end
 
     private
