75c5f6ef7685522ac98136151d809fbcd761e16a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-07-05 15:32:04 -0300

    TasksScheduler: setup with EacRubyUtils::RootModuleSetup.

--- lib/tasks_scheduler.rb
+++ lib/tasks_scheduler.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/require_sub'
+require 'eac_ruby_utils'
+EacRubyUtils::RootModuleSetup.perform __FILE__
 
 module TasksScheduler
-  ::EacRubyUtils.require_sub __FILE__
 end
+
+require 'avm/eac_generic_base0'
+require 'avm/eac_ruby_base1'
+require 'daemons'
+require 'parse-cron'
+require 'rails'
+
+require 'tasks_scheduler/engine'
