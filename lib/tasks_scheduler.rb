a918554607e5b3e81bf3950e6755bb980c09df87
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-03 21:23:53 -0300

    TasksScheduler: remove setup with EacRubyUtils::RootModuleSetup.

--- lib/tasks_scheduler.rb
+++ lib/tasks_scheduler.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
 module TasksScheduler
 end
-
-require 'avm/eac_generic_base0'
-require 'avm/eac_ruby_base1'
-require 'daemons'
-require 'parse-cron'
-require 'rails'
 
 require 'tasks_scheduler/engine'
