692b2fc40c7252041941d0580e502d87625cb57f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-12 20:47:23 -0300

    **/*.rb: remove requires desnecessários para "eac_rails_utils".

--- config/initializers/menus.rb
+++ config/initializers/menus.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'eac_rails_utils/patches/application'
-
 Rails.application.root_menu.group(:admin).group(:tasks_scheduler).within do |g|
   g.action :tasks_scheduler_daemon
   g.action :scheduled_tasks
