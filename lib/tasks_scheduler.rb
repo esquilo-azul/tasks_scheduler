9e60ddd3f2ae2e3717dae704d62d526612670c84
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-20 13:57:31 -0300

    TasksScheduler: setup with Rails::Engine.

--- lib/tasks_scheduler.rb
+++ lib/tasks_scheduler.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils'
-EacRubyUtils::RootModuleSetup.perform __FILE__
-
 module TasksScheduler
 end
-
-require 'avm/eac_ruby_base1'
-require 'eac_active_scaffold'
-require 'eac_rails_utils'
 
 require 'tasks_scheduler/engine'
--- lib/tasks_scheduler/engine.rb
+++ lib/tasks_scheduler/engine.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require 'avm/eac_ruby_base1'
+require 'eac_active_scaffold'
+require 'eac_rails_utils'
+require 'eac_ruby_utils'
+
 module TasksScheduler
   class Engine < ::Rails::Engine
     include ::EacRailsUtils::EngineHelper
