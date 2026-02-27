dad25dbebaf112ae15721f9d473271e656274064
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-27 15:07:26 -0300

    config/initializers/menus.rb: wrap code in `Rails.application.reloader.to_prepare`.

--- config/initializers/menus.rb
+++ config/initializers/menus.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-Rails.application.root_menu.group(:admin).group(:tasks_scheduler).within do |g|
-  g.action :tasks_scheduler_daemon
-  g.action :scheduled_tasks
-  g.action :scheduled_task_statuses
+Rails.application.reloader.to_prepare do
+  Rails.application.root_menu.group(:admin).group(:tasks_scheduler).within do |g|
+    g.action :tasks_scheduler_daemon
+    g.action :scheduled_tasks
+    g.action :scheduled_task_statuses
+  end
 end
